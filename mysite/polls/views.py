8d2c916adca1434de72ed35e16b134c2f9d5f8ad
xiechen0620 <xiechen0620@gmail.com>
2017-07-10 23:23:00 +0900

    Finish "Write a simple form" part of Tutorial 4

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 
 from .models import Question, Choice
 
+
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -12,13 +13,16 @@ def index(request):
     }
     return render(request, 'polls/index.html', context)
 
+
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
+
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
