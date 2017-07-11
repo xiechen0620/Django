4f0ac61e7f9c625a1e0efb2bb34b8838cd4f09af
xiechen0620 <xiechen0620@gmail.com>
2017-07-12 00:04:58 +0900

    Update from PyCharm

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,10 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 
 from .models import Question, Choice
+
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -11,7 +12,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        #Return the last five published questions.
+        # Return the last five published questions.
         return Question.objects.order_by('-pub_date')[:5]
 
 
@@ -32,13 +33,16 @@ def index(request):
     }
     return render(request, 'polls/index.html', context)
 
+
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
+
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
