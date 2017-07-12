b3ccb3bfacf3cd1736ecaee3b3b3c646615ffa11
xiechen0620 <xiechen0620@gmail.com>
2017-07-12 14:44:04 +0900

    Tutorial 6 finished

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse, timezone
+from django.utils import timezone
+from django.urls import reverse
 from django.views import generic
 
 from .models import Question, Choice
@@ -14,7 +15,7 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         # Return the last five published questions.
         # (not including those set to be published in the future.)
-        return Question.objcets.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -23,7 +24,7 @@ class DetailView(generic.DetailView):
 
     def get_queryset(self):
         # Excludes any questions that aren't published yet.
-        return Question.ojbects.filter(pub_date__lte=timezone.now())
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
